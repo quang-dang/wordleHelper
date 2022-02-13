2bdbc699d21e498752c9e40a7acd19d3e9b70cc7
Quang Dang <quangwdang@gmail.com>
2022-02-13 11:35:25 -0800

    Read from csv file and make a trie

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,31 @@
-from flask import render_template
+import csv
+from flask import current_app, render_template
 from app import app
 
+class Trie:
+    def __init__(self):
+        self.root = {}
+    
+    def add(self, word):
+        if not word:
+            return
+        node = self.root
+        for c in word:
+            if c not in node:
+                node[c] = {}
+            node = node[c]
+        node["*"] = word
+trie = Trie()
 @app.route('/')
 @app.route('/index')
 def index():
+    words = []
+    with current_app.open_resource('static/data.csv', "r") as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        for row in csv_reader:
+            for word in row:
+                words.append(word)
+                trie.add(word)
     user = {'username': 'Miguel'}
     posts = [
         {
@@ -15,4 +37,4 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', user=user, posts=posts, words = words)

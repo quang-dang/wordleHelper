a1575ade734371446961bf3efa483b184e71ff9e
Quang Dang <quangwdang@gmail.com>
2022-08-29 18:20:57 -0700

    Add color picker

---
+++ guess.js
@@ -0,0 +1,33 @@
+function validateInputText(evt) {
+    var theEvent = evt || window.event;
+  
+    // Handle paste
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+    // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[A-Za-z]/;
+    if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+    }  
+  }
+
+function changeInputColor(guessId, inputId, colorId) {
+    const elementId = "guess-".concat(guessId).concat("-input-").concat(inputId);
+    document.getElementById(elementId).style.backgroundColor = colorId;
+    ["green","yellow","gray"].forEach(cId => {
+        let colorElementId = "guess-".concat(guessId).concat("-input-").concat(inputId).concat("-color-").concat(cId);
+        let element = document.getElementById(colorElementId);
+        if (colorId === cId) {
+            element.style.borderStyle = "solid";
+            element.style.borderWidth = "0.1em";
+            element.style.borderColor = "black";
+        } else {
+            element.style.borderStyle = "none";
+        }
+    });
+}
